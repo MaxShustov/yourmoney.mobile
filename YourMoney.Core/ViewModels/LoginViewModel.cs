51ee9b72caef5cdfb50eed9a03025c20352b8624
Maksym Shustov <maksym_shustov@epam.com>
2017-06-10 23:21:20 +0300

    add view model

---
+++ YourMoney.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using YourMoney.Core.Services.Abstract;
+
+namespace YourMoney.Core.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private readonly IUserService _userService;
+
+        private string _userName;
+        private string _password;
+
+        public LoginViewModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                Set(() => UserName, ref _userName, value);
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                Set(() => Password, ref _password, value);
+            }
+        }
+
+        private async void Login()
+        {
+            await _userService.Login(UserName, Password);
+        }
+    }
+}
