864a7e95faf56192146364920287bed23cb5330b
Maksym Shustov <maksym_shustov@epam.com>
2017-06-10 23:24:33 +0300

    Add app start

---
+++ YourMoney.Core/AppStart.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using YourMoney.Core.ApiClients.Abstract;
+using YourMoney.Core.ApiClients.Implementation;
+using YourMoney.Core.Models;
+using YourMoney.Core.Services.Abstract;
+using YourMoney.Core.Services.Implementation;
+
+namespace YourMoney.Core
+{
+    public class AppStart
+    {
+        public static void Initialize()
+        {
+            SimpleIoc.Default.Register<IApiContext, ApiContext>();
+            SimpleIoc.Default.Register<IUserService, UserService>();
+
+            SimpleIoc.Default.Register<LoginModel>();
+        }
+    }
+}
