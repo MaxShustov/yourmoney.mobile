6b7208ac49c6a71f80d99e1a22fe10b657af5320
Maksym Shustov <maksym_shustov@epam.com>
2017-06-15 15:07:59 +0300

    Add splash screen to android

--- YourMoney.Droid/Activities/SplashScreenActivity.cs
+++ YourMoney.Droid/Activities/SplashScreenActivity.cs
@@ -3,14 +3,13 @@ using Android.OS;
 
 namespace YourMoney.Droid.Activities
 {
-    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/SplashTheme")]
     public class SplashScreenActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
         }
     }
