9466fba4e6c32dd073df85a168384f370df4241b
Maksym Shustov <maksym_shustov@epam.com>
2017-06-15 15:49:18 +0300

    Add basic login page

--- YourMoney.Droid/Activities/SplashScreenActivity.cs
+++ YourMoney.Droid/Activities/SplashScreenActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System.Threading.Tasks;
+using Android.App;
 using Android.OS;
 
 namespace YourMoney.Droid.Activities
@@ -6,11 +7,20 @@ namespace YourMoney.Droid.Activities
     [Activity(MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/SplashTheme")]
     public class SplashScreenActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
+
+            await GoToLogin();
+        }
+
+        private async Task GoToLogin()
+        {
+            await Task.Delay(1000);
+
+            StartActivity(typeof(LoginActivity));
         }
     }
 }
