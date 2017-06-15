cfd879e6bbbc95f68bcf40bc13e6204927360849
Maksym Shustov <maksym_shustov@epam.com>
2017-06-15 14:18:29 +0300

    Add icons to droid app

---
+++ YourMoney.Droid/Activities/SplashScreenActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace YourMoney.Droid.Activities
+{
+    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
