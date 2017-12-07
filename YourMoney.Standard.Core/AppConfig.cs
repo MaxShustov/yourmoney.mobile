2d831a0ec37ca5ac9361b00cd401bce7087e4c67
Maksym Shustov <max.shustov@hotmail.com>
2017-12-07 22:03:08 +0200

    Fix api url

--- YourMoney.Standard.Core/AppConfig.cs
+++ YourMoney.Standard.Core/AppConfig.cs
@@ -2,7 +2,7 @@
 {
     public class AppConfig
     {
-        public const string BaseUrl = "https://your-money.herokuapp.com";
+        public const string BaseUrl = "https://your-money-api.herokuapp.com";
         //public const string BaseUrl = "http://localhost:53467";
 
         public static readonly string ApiUrl = $"{BaseUrl}/api";
