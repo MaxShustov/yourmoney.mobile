886b3c3c7bf9586af31a7f752d1b1851864f7512
Maksym Shustov <Maksym_Shustov@epam.com>
2018-05-15 15:22:48 +0300

    Add simple Rx test

--- YourMoney.Core.Tests.New/UserServiceTests.cs
+++ YourMoney.Core.Tests.New/UserServiceTests.cs
@@ -1,13 +1,37 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Microsoft.Reactive.Testing;
+using Moq;
+using YourMoney.Standard.Core.Api.Interfaces;
+using YourMoney.Standard.Core.Api.Models;
+using YourMoney.Standard.Core.Services.Abstract;
+using YourMoney.Standard.Core.Services.Implementation;
+
 namespace YourMoney.Core.Tests.New
 {
-    [TestFixture()]
-    public class UserServiceTests
+    [TestFixture]
+    public class UserServiceTests: ReactiveTest
     {
-        [Test()]
+        [Test]
         public void TestCase()
         {
+            var scheduler = new TestScheduler();
+
+            var settingMock = new Mock<ISettingService>();
+            var userApi = new Mock<IUsersApi>();
+            var observable = Observable.Return(new LoginResponseModel()).Delay(TimeSpan.FromTicks(1), scheduler);
+            userApi.Setup(api => api.Login(It.IsAny<LoginRequestModel>())).Returns(observable);
+
+            var observer = scheduler.CreateObserver<Unit>();
+            var userService = new UserService(settingMock.Object, userApi.Object);
+            userService.Login(string.Empty, string.Empty)
+                .Subscribe(observer);
+            
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(2).Ticks);
+
+            Assert.AreEqual(1, observer.Messages.Count);
         }
     }
 }
